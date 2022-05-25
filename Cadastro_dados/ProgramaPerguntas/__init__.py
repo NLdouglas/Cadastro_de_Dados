91f8672346c937dd1368919ce107be35e572c31d
Douglas <87586242+NLdouglas@users.noreply.github.com>
2022-05-24 22:33:56 -0300

    sistema perguntas pront

--- Cadastro_dados/ProgramaPerguntas/__init__.py
+++ Cadastro_dados/ProgramaPerguntas/__init__.py
@@ -1,15 +1,35 @@
+# FAZER TEXTO EXPLICATIVO NA INTERFACE OU EM UM MODULO SEPARADO
+from time import sleep
+
 perguntas = {
     'Pergunta 1': {
         'pergunta': 'Quanto é 2+2',
-        'resposta': {'a': '1', 'b': '4', 'c': '5'},
-        'resposta_certa': 'b'
+        'respostas': {'a': '1', 'b': '4', 'c': '5',},
+        'resposta_certa': 'b',
     },
     'Pergunta 2': {
-        'Pergunta': 'Quanto é 3*2? ',
-        'respostas': {'a': '4', 'b': '10', 'c': '6'},
+        'pergunta': 'Quanto é 3*2? ',
+        'respostas': {'a': '4', 'b': '10', 'c': '6',},
         'resposta_certa': 'c',
-    }
+    },
 }
+print()
 
-for pk, pv in perguntas.items():
-    print(f'{pk}: {pv["perguntas"]}')
+respostas_certas = 0
+for pk, pv in perguntas.items(): # pk = perguntas keys, pv = perguntas values
+    print(f'{pk}: {pv["pergunta"]}')
+
+    print('Escolha uma das opções abaixo: (Digite: A, B OU C)')
+    for rk, rv in pv["respostas"].items():
+        print(f'[{rk}]: {rv}')
+    print()
+    sleep(1)
+    resposta_usuario = input('Digite a opção desejada(A, B ou C): ')
+
+    if resposta_usuario == pv['resposta_certa']:
+        print('Você acertou!!')
+        respostas_certas += 1
+    else:
+        print('Você errou!!')
+    print()
+    sleep(1.5)
--- Cadastro_dados/main.py
+++
@@ -1,4 +0,0 @@
-from interface import* # FAZER PARTE DA INTERFACE
-
-while True:
-    
